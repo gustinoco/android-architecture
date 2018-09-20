0b87b40244ee430bbf47509679980940284354d5
Gustavo <gustavo.tinoco@e-deploy.com.br>
2018-09-20 10:44:10 -0400

    Adjust plugins updated and add new pluggins

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/addedittask/AddEditTaskFragment.kt
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/addedittask/AddEditTaskFragment.kt
@@ -48,9 +48,9 @@ class AddEditTaskFragment : Fragment(), AddEditTaskContract.View {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        with(activity.findViewById<FloatingActionButton>(R.id.fab_edit_task_done)) {
-            setImageResource(R.drawable.ic_done)
-            setOnClickListener {
+        with(activity?.findViewById<FloatingActionButton>(R.id.fab_edit_task_done)) {
+            this?.setImageResource(R.drawable.ic_done)
+            this?.setOnClickListener {
                 presenter.saveTask(title.text.toString(), description.text.toString())
             }
         }
@@ -73,8 +73,8 @@ class AddEditTaskFragment : Fragment(), AddEditTaskContract.View {
 
     override fun showTasksList() {
         with(activity) {
-            setResult(Activity.RESULT_OK)
-            finish()
+            this?.setResult(Activity.RESULT_OK)
+            this?.finish()
         }
     }
 
--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragment.kt
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragment.kt
@@ -98,9 +98,9 @@ class TasksFragment : Fragment(), TasksContract.View {
             // Set up progress indicator
             findViewById<ScrollChildSwipeRefreshLayout>(R.id.refresh_layout).apply {
                 setColorSchemeColors(
-                        ContextCompat.getColor(activity, R.color.colorPrimary),
-                        ContextCompat.getColor(activity, R.color.colorAccent),
-                        ContextCompat.getColor(activity, R.color.colorPrimaryDark)
+                        ContextCompat.getColor(activity?.applicationContext!!, R.color.colorPrimary),
+                        ContextCompat.getColor(activity?.applicationContext!!, R.color.colorAccent),
+                        ContextCompat.getColor(activity?.applicationContext!!, R.color.colorPrimaryDark)
                 )
                 // Set the scrolling view in the custom SwipeRefreshLayout.
                 scrollUpChild = listView
@@ -120,7 +120,7 @@ class TasksFragment : Fragment(), TasksContract.View {
         }
 
         // Set up floating action button
-        activity.findViewById<FloatingActionButton>(R.id.fab_add_task).apply {
+        activity?.findViewById<FloatingActionButton>(R.id.fab_add_task)?.apply {
             setImageResource(R.drawable.ic_add)
             setOnClickListener { presenter.addNewTask() }
         }
@@ -143,7 +143,7 @@ class TasksFragment : Fragment(), TasksContract.View {
     }
 
     override fun showFilteringPopUpMenu() {
-        PopupMenu(context, activity.findViewById(R.id.menu_filter)).apply {
+        PopupMenu(this.context!!, activity?.findViewById(R.id.menu_filter)!!).apply {
             menuInflater.inflate(R.menu.filter_tasks, menu)
             setOnMenuItemClickListener { item ->
                 when (item.itemId) {
